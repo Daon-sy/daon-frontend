bb4b42a36d47e599bebea9a748471e5d52db2f5f
sungeunnn <fronteun@naver.com>
2023-10-31 10:22:42 +0900

    ❇️[feat] 초기 router 설정 #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import Home from "pages/Home"
+import Landing from "pages/Landing"
+
 import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
-  return <div className="App">.</div>
+  const isLogin = false
+
+  return (
+    <BrowserRouter>
+      {isLogin ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      )}
+    </BrowserRouter>
+  )
 }
 
 export default App
