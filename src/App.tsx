de534ecc2c54a27fa58727d1b730223682da5422
sungeunnn <fronteun@naver.com>
2023-10-31 20:25:59 +0900

    💄[style] styled-reset 적용 #8

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import React from "react"
 import Home from "pages/Home"
 import Landing from "pages/Landing"
+import { Reset } from "styled-reset"
 import UserLayout from "Layouts/UserLayout"
 
 function App() {
   const isLogin = true
 
   return (
-    <BrowserRouter>
-      <Routes>
-        {isLogin ? (
-          <Route element={<UserLayout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        ) : (
-          <Route path="/" element={<Landing />} />
-        )}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Reset />
+      <BrowserRouter>
+        <Routes>
+          {isLogin ? (
+            <Route element={<UserLayout />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+          ) : (
+            <Route path="/" element={<Landing />} />
+          )}
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
