fec49bac468245c67966a378253152d0f6ac147d
sungeunnn <fronteun@naver.com>
2023-11-01 16:38:01 +0900

    💄[style] 사이드바 사용자 레이아웃에 적용 #20

--- src/Layouts/UserLayout.tsx
+++ src/Layouts/UserLayout.tsx
@@ -2,13 +2,16 @@ import React from "react"
 import Header from "components/Header/Header"
 import { Outlet } from "react-router-dom"
 import "./Layout.css"
+import Sidebar from "components/Sidebar/Sidebar"
 
 const UserLayout = () => {
   return (
-    <div className="default-layout">
+    <div className="default_layout">
       <Header />
       <main className="main">
-        <div className="side-nav" />
+        <div className="side_bar">
+          <Sidebar />
+        </div>
         <div className="page">
           <Outlet />
         </div>
