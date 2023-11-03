097083ee5e6b61838618292716376fd4ede7fb52
sungeunnn <fronteun@naver.com>
2023-11-03 14:19:29 +0900

    ❇️[feat] 프로젝트 생성 기능 구현 #33
    - formData 상태관리
    - 프로젝트 생성/Error 메세지

--- src/components/modal/createProject/CreateProjectModal.tsx
+++ src/components/modal/createProject/CreateProjectModal.tsx
@@ -4,6 +4,8 @@ import Modal from "@mui/material/Modal"
 import Typography from "@mui/material/Typography"
 import { Stack, TextField } from "@mui/material"
 import Button from "@mui/material/Button"
+import { createProjectApi } from "api/projectApi"
+import { useAlert } from "hooks/useAlert"
 
 interface CreateProjectProps {
   open: boolean
@@ -21,10 +23,59 @@ const style = {
   p: 4,
 }
 
+interface CreateProjectForm {
+  projectName: string
+  description: string
+}
+
 const CreateProjectModal: React.FC<CreateProjectProps> = (
   props: CreateProjectProps,
 ) => {
   const { open, handleClose } = props
+  const [formData, setFormData] = React.useState<CreateProjectForm>({
+    projectName: "",
+    description: "",
+  })
+  const { addSuccess, addError } = useAlert()
+
+  const onProjectNameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      projectName: e.target.value,
+    })
+  }
+
+  const onProjectDescriptionChanged = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setFormData({
+      ...formData,
+      description: e.target.value,
+    })
+  }
+
+  const onValidateBtn = () => {
+    if (formData.projectName.length === 0) {
+      addError("프로젝트 제목은 필수입력 값입니다")
+
+      return
+    }
+
+    createProjectApi({
+      projectName: formData.projectName,
+      description: formData.description,
+    })
+      .then(res => {
+        if (res.status === 201) {
+          addSuccess("프로젝트 생성 성공!")
+        }
+      })
+      .catch(err => {
+        if (err.response >= 500) {
+          addError("서버 오류입니다. 다시 시도해주세요")
+        }
+      })
+  }
 
   return (
     <div>
@@ -46,12 +97,20 @@ const CreateProjectModal: React.FC<CreateProjectProps> = (
           </Typography>
           <Stack spacing={2}>
             <Typography variant="h6">프로젝트 정보 입력</Typography>
-            <TextField required label="프로젝트 이름" variant="outlined" />
+            <TextField
+              required
+              label="프로젝트 이름"
+              variant="outlined"
+              value={formData.projectName}
+              onChange={onProjectNameChanged}
+            />
             <TextField
               multiline
               rows={5}
               label="프로젝트 설명"
               variant="outlined"
+              value={formData.description}
+              onChange={onProjectDescriptionChanged}
             />
           </Stack>
           <Stack
@@ -62,10 +121,20 @@ const CreateProjectModal: React.FC<CreateProjectProps> = (
               mt: 2,
             }}
           >
-            <Button fullWidth size="large" variant="outlined">
+            <Button
+              fullWidth
+              size="large"
+              variant="outlined"
+              onClick={onValidateBtn}
+            >
               확인
             </Button>
-            <Button fullWidth size="large" variant="contained">
+            <Button
+              fullWidth
+              size="large"
+              variant="contained"
+              onClick={handleClose}
+            >
               취소
             </Button>
           </Stack>
