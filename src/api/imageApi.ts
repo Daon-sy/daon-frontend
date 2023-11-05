69265e8a5b52a59780923e2a92320e0a8560d76c
hi0yoo <yoo971202@naver.com>
2023-11-05 17:57:13 +0900

    🔨[refactor] API response 타입 지정 #35

--- src/api/imageApi.ts
+++ src/api/imageApi.ts
@@ -1,4 +1,5 @@
-import { authAxios } from "api/index"
+import { ApiResponse, authAxios } from "api/index"
+import { AxiosResponse } from "axios"
 
 export interface ImageUploadRequest {
   image: File
@@ -8,13 +9,14 @@ export interface ImageUploadResponse {
   imageUrl: string
 }
 
-export const imageUploadApi = async (request: ImageUploadRequest) => {
+export const imageUploadApi = async (
+  request: ImageUploadRequest,
+): Promise<AxiosResponse<ApiResponse<ImageUploadResponse>>> => {
   const axiosConfig = {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   }
-
   const formData = new FormData()
   formData.append("image", request.image)
 
--- src/api/signInApi.ts
+++ src/api/signInApi.ts
@@ -1,10 +1,13 @@
-import { basicAxios } from "api/index"
+import { ApiResponse, basicAxios } from "api/index"
+import { AxiosResponse } from "axios"
 
 export interface SignInRequest {
   email: string
   password: string
 }
 
-export const signInApi = async (signInRequest: SignInRequest) => {
+export const signInApi = async (
+  signInRequest: SignInRequest,
+): Promise<AxiosResponse<ApiResponse>> => {
   return basicAxios.post("/api/sign-in", signInRequest)
 }
