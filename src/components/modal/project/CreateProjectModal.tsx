3fb60427402fd893f7f4d45aa5aebfb0325508d3
sungeunnn <fronteun@naver.com>
2023-11-07 09:53:27 +0900

    🔨[refactor] CustomModal 적용 #49

--- src/components/modal/project/CreateProjectModal.tsx
+++ src/components/modal/project/CreateProjectModal.tsx
@@ -1,26 +1,15 @@
 import * as React from "react"
-import Box from "@mui/material/Box"
-import Modal from "@mui/material/Modal"
 import Typography from "@mui/material/Typography"
 import { Stack, TextField } from "@mui/material"
 import Button from "@mui/material/Button"
 import { createProjectApi } from "api/projectApi"
 import { useAlert } from "hooks/useAlert"
+import CustomModal from "components/common/CustomModal"
 
 interface CreateProjectProps {
   open: boolean
   handleClose: () => void
-}
-const style = {
-  position: "absolute" as const,
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
+  cleanUp: () => void
 }
 
 interface CreateProjectForm {
@@ -31,7 +20,7 @@ interface CreateProjectForm {
 const CreateProjectModal: React.FC<CreateProjectProps> = (
   props: CreateProjectProps,
 ) => {
-  const { open, handleClose } = props
+  const { open, handleClose, cleanUp } = props
   const [formData, setFormData] = React.useState<CreateProjectForm>({
     projectName: "",
     description: "",
@@ -79,67 +68,65 @@ const CreateProjectModal: React.FC<CreateProjectProps> = (
 
   return (
     <div>
-      <Modal
+      <CustomModal
         open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        handleClose={handleClose}
+        cleanUp={cleanUp}
+        width={400}
       >
-        <Box sx={style}>
-          <Typography
-            variant="h4"
-            align="center"
-            sx={{
-              paddingBottom: 2,
-            }}
+        <Typography
+          variant="h4"
+          align="center"
+          sx={{
+            paddingBottom: 2,
+          }}
+        >
+          프로젝트 생성
+        </Typography>
+        <Stack spacing={2}>
+          <Typography variant="h6">프로젝트 정보 입력</Typography>
+          <TextField
+            required
+            label="프로젝트 이름"
+            variant="outlined"
+            value={formData.projectName}
+            onChange={onProjectNameChanged}
+          />
+          <TextField
+            multiline
+            rows={5}
+            label="프로젝트 설명"
+            variant="outlined"
+            value={formData.description}
+            onChange={onProjectDescriptionChanged}
+          />
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{
+            height: 50,
+            mt: 2,
+          }}
+        >
+          <Button
+            fullWidth
+            size="large"
+            variant="outlined"
+            onClick={onValidateBtn}
           >
-            프로젝트 생성
-          </Typography>
-          <Stack spacing={2}>
-            <Typography variant="h6">프로젝트 정보 입력</Typography>
-            <TextField
-              required
-              label="프로젝트 이름"
-              variant="outlined"
-              value={formData.projectName}
-              onChange={onProjectNameChanged}
-            />
-            <TextField
-              multiline
-              rows={5}
-              label="프로젝트 설명"
-              variant="outlined"
-              value={formData.description}
-              onChange={onProjectDescriptionChanged}
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={2}
-            sx={{
-              height: 50,
-              mt: 2,
-            }}
+            확인
+          </Button>
+          <Button
+            fullWidth
+            size="large"
+            variant="contained"
+            onClick={handleClose}
           >
-            <Button
-              fullWidth
-              size="large"
-              variant="outlined"
-              onClick={onValidateBtn}
-            >
-              확인
-            </Button>
-            <Button
-              fullWidth
-              size="large"
-              variant="contained"
-              onClick={handleClose}
-            >
-              취소
-            </Button>
-          </Stack>
-        </Box>
-      </Modal>
+            취소
+          </Button>
+        </Stack>
+      </CustomModal>
     </div>
   )
 }
