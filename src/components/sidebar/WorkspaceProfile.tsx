5818be5944762da724b3560f226f266d3d231655
sungeunnn <fronteun@naver.com>
2023-11-21 10:27:47 +0900

    💄[style] Sidebar 워크스페이스 프로필 스타일 적용 #105

--- src/components/sidebar/WorkspaceProfile.tsx
+++ src/components/sidebar/WorkspaceProfile.tsx
@@ -1,68 +1,103 @@
 import * as React from "react"
-import styled from "styled-components"
-import { Box, Menu, MenuItem, Typography } from "@mui/material"
+import { Box } from "@mui/material"
 import { TEST_IMAGE_URL } from "env"
-import ShowMember from "components/workspace/modal/WorkspaceParticipantsModal"
-
-const WorkspaceProfileWrapper = styled.button`
-  box-sizing: border-box;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-  padding: 16px 0;
-  position: relative;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  margin-bottom: 4px;
-  border: none;
-`
 
 const WorkSpaceProfile: React.FC = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null,
-  )
-  const [openShowModal, setOpenShowModal] = React.useState<boolean>(false)
-
-  const handleOpenShowModal = () => {
-    setOpenShowModal(true)
-  }
-
-  const handleCloseShowModal = () => {
-    setOpenShowModal(false)
-  }
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    if (!anchorElUser) {
-      setAnchorElUser(event.currentTarget)
-    }
-  }
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
-  }
-
   return (
-    <>
-      <WorkspaceProfileWrapper onClick={handleOpenUserMenu}>
+    <Box
+      sx={{
+        boxSizing: "border-box",
+        width: "100%",
+        height: "30%",
+        bgcolor: "#82b89b",
+        paddingY: "16px",
+        position: "relative",
+        cursor: " pointer",
+        display: "flex",
+        justifyContent: "center",
+        mb: "4px",
+        border: "none",
+        borderBottomLeftRadius: "20px",
+        borderBottomRightRadius: "20px",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+        }}
+      >
         <Box
           sx={{
-            width: "90%",
-            height: 180,
-            border: 2,
-            borderRadius: 10,
             display: "flex",
+            flexDirection: "column",
             alignItems: "center",
-            justifyContent: "center",
+            justifyContent: "space-between",
+            width: "100%",
           }}
         >
           <Box
+            component="div"
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
+              borderRadius: 1,
+              width: "100%",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              fontSize: 20,
+              mb: "4px",
+              textAlign: "center",
+              color: "#476354",
+              fontWeight: "bold",
             }}
           >
+            워크스페이스 이름
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              width: 140,
+              height: 140,
+              background: "linear-gradient( #ffffff, #faa788)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 50,
+            }}
+          >
+            <Box
+              component="img"
+              src={`${TEST_IMAGE_URL}`}
+              sx={{
+                objectFit: "cover",
+                width: 120,
+                height: 120,
+                borderRadius: 50,
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              mt: "2px",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            <Box
+              sx={{
+                borderRadius: 1,
+                width: "100%",
+                fontSize: 20,
+                fontWeight: "bold",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+              }}
+            >
+              닉네임
+            </Box>
             <Box
               sx={{
                 borderRadius: 1,
@@ -70,85 +105,18 @@ const WorkSpaceProfile: React.FC = () => {
                 overflow: "hidden",
                 textOverflow: "ellipsis",
                 whiteSpace: "nowrap",
+                color: "#b6d1c2",
+                marginY: 1,
                 fontSize: 18,
-                mb: "4px",
+                fontWeight: "bold",
               }}
             >
-              워크스페이스 이름
-            </Box>
-            <Box
-              component="img"
-              src={`${TEST_IMAGE_URL}`}
-              sx={{
-                objectFit: "cover",
-                width: 80,
-                height: 80,
-                borderRadius: 50,
-              }}
-            />
-            <Box sx={{ width: 180, maxHeight: 140, mt: "2px" }}>
-              <Box
-                sx={{
-                  borderRadius: 1,
-                  width: "100%",
-                  fontSize: 16,
-                  fontWeight: "bold",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                닉네임
-              </Box>
-              <Box
-                sx={{
-                  borderRadius: 1,
-                  width: "100%",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                  color: "rgba(0,0,0,0.6)",
-                }}
-              >
-                workspace@gmail.com
-              </Box>
+              workspace@gmail.com
             </Box>
           </Box>
         </Box>
-        <Menu
-          sx={{ mt: "45px" }}
-          id="menu-appbar"
-          anchorEl={anchorElUser}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "center",
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
-          open={Boolean(anchorElUser)}
-          onClick={handleCloseUserMenu}
-        >
-          <MenuItem onClick={handleOpenShowModal}>
-            <Typography textAlign="center" id="showMember">
-              구성원보기
-            </Typography>
-          </MenuItem>
-          <MenuItem>
-            <Typography textAlign="center">프로필 변경</Typography>
-          </MenuItem>
-          <MenuItem>
-            <Typography textAlign="center">워크스페이스 생성</Typography>
-          </MenuItem>
-          <MenuItem>
-            <Typography textAlign="center">워크스페이스 변경</Typography>
-          </MenuItem>
-        </Menu>
-      </WorkspaceProfileWrapper>
-      <ShowMember open={openShowModal} handleClose={handleCloseShowModal} />
-    </>
+      </Box>
+    </Box>
   )
 }
 
