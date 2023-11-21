0cc4c172e8f96cc072e193ea9039ef0e977d68ba
sungeunnn <fronteun@naver.com>
2023-11-21 14:27:11 +0900

    💄[style] Sidebar 아이콘버튼 스타일 적용 #105

--- src/components/sidebar/WorkspaceProfile.tsx
+++ src/components/sidebar/WorkspaceProfile.tsx
@@ -8,112 +8,95 @@ const WorkSpaceProfile: React.FC = () => {
       sx={{
         boxSizing: "border-box",
         width: "100%",
-        height: "30%",
+        height: "75%",
+        minHeight: "230px",
         bgcolor: "#82b89b",
-        paddingY: "16px",
-        position: "relative",
+        position: "relatvie",
         cursor: " pointer",
-        display: "flex",
-        justifyContent: "center",
-        mb: "4px",
         border: "none",
         borderBottomLeftRadius: "20px",
         borderBottomRightRadius: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
       }}
     >
       <Box
+        component="div"
+        sx={{
+          borderRadius: 1,
+          width: "100%",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          fontSize: 20,
+          textAlign: "center",
+          color: "#476354",
+          fontWeight: "bold",
+        }}
+      >
+        워크스페이스 이름
+      </Box>
+      <Box
+        component="div"
+        sx={{
+          width: "130px",
+          height: "130px",
+          background: "linear-gradient( #ffffff, #faa788)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 50,
+        }}
+      >
+        <Box
+          component="img"
+          src={`${TEST_IMAGE_URL}`}
+          sx={{
+            objectFit: "cover",
+            width: "110px",
+            height: "110px",
+            borderRadius: 50,
+          }}
+        />
+      </Box>
+      <Box
         sx={{
           width: "100%",
-          height: "100%",
-          display: "flex",
+          mt: "2px",
+          textAlign: "center",
+          color: "white",
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "space-between",
+            borderRadius: 1,
             width: "100%",
+            fontSize: 20,
+            fontWeight: "bold",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
           }}
         >
-          <Box
-            component="div"
-            sx={{
-              borderRadius: 1,
-              width: "100%",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              whiteSpace: "nowrap",
-              fontSize: 20,
-              mb: "4px",
-              textAlign: "center",
-              color: "#476354",
-              fontWeight: "bold",
-            }}
-          >
-            워크스페이스 이름
-          </Box>
-          <Box
-            component="div"
-            sx={{
-              width: 140,
-              height: 140,
-              background: "linear-gradient( #ffffff, #faa788)",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: 50,
-            }}
-          >
-            <Box
-              component="img"
-              src={`${TEST_IMAGE_URL}`}
-              sx={{
-                objectFit: "cover",
-                width: 120,
-                height: 120,
-                borderRadius: 50,
-              }}
-            />
-          </Box>
-          <Box
-            sx={{
-              width: "100%",
-              mt: "2px",
-              textAlign: "center",
-              color: "white",
-            }}
-          >
-            <Box
-              sx={{
-                borderRadius: 1,
-                width: "100%",
-                fontSize: 20,
-                fontWeight: "bold",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                whiteSpace: "nowrap",
-              }}
-            >
-              닉네임
-            </Box>
-            <Box
-              sx={{
-                borderRadius: 1,
-                width: "100%",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                whiteSpace: "nowrap",
-                color: "#b6d1c2",
-                marginY: 1,
-                fontSize: 18,
-                fontWeight: "bold",
-              }}
-            >
-              workspace@gmail.com
-            </Box>
-          </Box>
+          닉네임
+        </Box>
+        <Box
+          sx={{
+            borderRadius: 1,
+            width: "100%",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            color: "#b6d1c2",
+            marginY: 1,
+            fontSize: 18,
+            lineHeight: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          workspace@gmail.com
         </Box>
       </Box>
     </Box>
