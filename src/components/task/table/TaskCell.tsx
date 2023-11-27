6bd25a766754bfc2a54d3d0beb73ed881673cf15
hi0yoo <yoo971202@naver.com>
2023-11-27 11:10:23 +0900

    ❇️[feat] 할 일 목록 테이블 뷰 기능 적용 #119
    - 클릭시 할 일 상세 모달 오픈
    - 북마크 아이콘 변경 및 북마크 기능 적용

--- src/components/task/table/TaskCell.tsx
+++ src/components/task/table/TaskCell.tsx
@@ -1,35 +1,11 @@
 import React from "react"
+import { Avatar, Chip, Stack, Box } from "@mui/material"
 import { TaskSummary } from "_types/task"
-import Box from "@mui/material/Box"
-import { Avatar, Chip, Stack, ToggleButton } from "@mui/material"
-import StarIcon from "@mui/icons-material/Star"
-import StarBorderIcon from "@mui/icons-material/StarBorder"
+import { getWorkspaceStore } from "store/userStore"
+import { getTaskDetailViewStore } from "store/taskStore"
 import { getTaskTableStore } from "store/taskTableStore"
-import TaskDetailModal from "components/task/modal/TaskDetailModal"
-
-// 북마크 버튼 아이콘
-interface BookmarkButtonProps {
-  selected: boolean
-}
-
-const BookmarkButton = ({ selected }: BookmarkButtonProps) => {
-  const iconSx = {
-    fontSize: "small",
-  }
-
-  return (
-    <ToggleButton
-      value="check"
-      selected={selected}
-      size="small"
-      sx={{
-        padding: 0.5,
-      }}
-    >
-      {selected ? <StarIcon sx={iconSx} /> : <StarBorderIcon sx={iconSx} />}
-    </ToggleButton>
-  )
-}
+import TaskBookmarkButton from "components/task/TaskBookmarkButton"
+import useHandleBookmark from "hooks/task/useHandleBookmark"
 
 interface Props {
   task: TaskSummary
@@ -44,122 +20,135 @@ const TaskCell: React.FC<Props> = ({ task, borderColor }: Props) => {
     endDateCellWidth,
     taskManagerCellWidth,
   } = cellWidth
-  const [detailModalOpen, setDetailModalOpen] = React.useState(false)
+
+  const { workspace } = getWorkspaceStore()
+  const { setTaskDetailParam } = getTaskDetailViewStore()
+  const { bookmarked, handleBookmark } = useHandleBookmark(
+    {
+      workspaceId: workspace?.workspaceId || 0,
+      projectId: task.project.projectId,
+      taskId: task.taskId,
+    },
+    task.bookmark,
+  )
 
   return (
-    <>
-      <Box>
-        <Stack
-          direction="row"
+    <Box>
+      <Stack
+        direction="row"
+        sx={{
+          border: "1px solid",
+          borderColor,
+          marginY: "-1px",
+          marginX: "-1px",
+          "&:hover": {
+            cursor: "pointer",
+          },
+        }}
+        onClick={() =>
+          setTaskDetailParam({
+            workspaceId: workspace?.workspaceId || 0,
+            projectId: task.project.projectId,
+            taskId: task.taskId,
+          })
+        }
+      >
+        <Box
           sx={{
+            width: 40,
+            height: 40,
             border: "1px solid",
             borderColor,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
             marginY: "-1px",
-            marginX: "-1px",
+            marginLeft: "-1px",
+            fontSize: 14,
+            fontWeight: 400,
           }}
         >
-          <Box
-            sx={{
-              width: 40,
-              height: 40,
-              border: "1px solid",
-              borderColor,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              marginY: "-1px",
-              marginLeft: "-1px",
-              fontSize: 14,
-              fontWeight: 400,
-            }}
-          >
-            {task.taskId}
-          </Box>
-          <Box
-            sx={{
-              width: titleCellWidth,
-              paddingX: 1,
-              border: "1px solid",
-              borderColor,
-              display: "flex",
-              alignItems: "center",
-              marginY: "-1px",
-              marginLeft: "-1px",
-              fontWeight: 400,
-            }}
-          >
-            <Box flexGrow={1}>{task.title}</Box>
-            <Box sx={{ display: "flex" }}>
-              <Box sx={{ marginRight: 1 }}>
-                <BookmarkButton selected={task.bookmark} />
-              </Box>
+          {task.taskId}
+        </Box>
+        <Box
+          sx={{
+            width: titleCellWidth,
+            paddingX: 1,
+            border: "1px solid",
+            borderColor,
+            display: "flex",
+            alignItems: "center",
+            marginY: "-1px",
+            marginLeft: "-1px",
+            fontWeight: 400,
+          }}
+        >
+          <Box flexGrow={1}>{task.title}</Box>
+          <Box sx={{ display: "flex" }}>
+            <Box sx={{ marginRight: 1 }}>
+              <TaskBookmarkButton
+                bookmarked={bookmarked}
+                handleClick={handleBookmark}
+              />
             </Box>
           </Box>
-          <Box
-            key={task.board?.boardId}
-            sx={{
-              width: boardCellWidth,
-              paddingX: 1,
-              border: "1px solid",
-              borderColor,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              marginY: "-1px",
-              marginLeft: "-1px",
-            }}
-          >
-            <Chip label={task.board?.title} color="primary" size="small" />
+        </Box>
+        <Box
+          key={task.board?.boardId}
+          sx={{
+            width: boardCellWidth,
+            paddingX: 1,
+            border: "1px solid",
+            borderColor,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginY: "-1px",
+            marginLeft: "-1px",
+          }}
+        >
+          <Chip label={task.board?.title} color="primary" size="small" />
+        </Box>
+        <Box
+          sx={{
+            width: endDateCellWidth,
+            paddingX: 1,
+            paddingY: 1,
+            color: "gray",
+            fontSize: 12,
+            border: "1px solid",
+            borderColor,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginY: "-1px",
+            marginLeft: "-1px",
+          }}
+        >
+          ~ {task.endDate}
+        </Box>
+        <Box
+          sx={{
+            width: taskManagerCellWidth,
+            paddingLeft: 1,
+            paddingRight: 2,
+            border: "1px solid",
+            borderColor,
+            display: "flex",
+            alignItems: "center",
+            marginY: "-1px",
+            marginLeft: "-1px",
+          }}
+        >
+          <Box>
+            <Avatar sx={{ width: 26, height: 26 }} />
           </Box>
-          <Box
-            sx={{
-              width: endDateCellWidth,
-              paddingX: 1,
-              paddingY: 1,
-              color: "gray",
-              fontSize: 12,
-              border: "1px solid",
-              borderColor,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              marginY: "-1px",
-              marginLeft: "-1px",
-            }}
-          >
-            ~ {task.endDate}
+          <Box sx={{ marginLeft: 1, fontSize: 14 }}>
+            {task.taskManager?.name}
           </Box>
-          <Box
-            sx={{
-              width: taskManagerCellWidth,
-              paddingLeft: 1,
-              paddingRight: 2,
-              border: "1px solid",
-              borderColor,
-              display: "flex",
-              alignItems: "center",
-              marginY: "-1px",
-              marginLeft: "-1px",
-            }}
-          >
-            <Box>
-              <Avatar sx={{ width: 26, height: 26 }} />
-            </Box>
-            <Box sx={{ marginLeft: 1, fontSize: 14 }}>
-              {task.taskManager?.name}
-            </Box>
-          </Box>
-        </Stack>
-      </Box>
-      {detailModalOpen ? (
-        <TaskDetailModal
-          projectId={task.project.projectId}
-          taskId={task.taskId}
-          open={detailModalOpen}
-          handleClose={() => setDetailModalOpen(false)}
-        />
-      ) : null}
-    </>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
