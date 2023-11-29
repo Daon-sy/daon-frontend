dcc1e53fca7631a222817b5291ceef72099b4581
sungeunnn <fronteun@naver.com>
2023-11-29 14:18:09 +0900

    💄[style] 워크스페이스 메인 SectionTitleWrapper 컴포넌트 #105
    - 각 section 마다 제목이 있고 똑같은 모양이기 때문에 컴포넌트로 뺌
    - prop은 width, color, icon, title, children은 필수입력값
    - maxWidth, height, pl은 선택 입력 값
    - 기능은 없음

---
+++ src/components/workspace/main/SectionTitleWrapper.tsx
@@ -0,0 +1,73 @@
+import React, { ReactNode } from "react"
+import { Box } from "@mui/material"
+import {
+  FontAwesomeIcon,
+  FontAwesomeIconProps,
+} from "@fortawesome/react-fontawesome"
+
+interface SectionTitleWrapperProp {
+  width: number | string
+  maxWidth?: string
+  height?: number | string
+  color: string
+  icon: FontAwesomeIconProps["icon"]
+  children?: ReactNode
+  title: string
+  pl?: number
+}
+const SectionTitleWrapper: React.FC<SectionTitleWrapperProp> = ({
+  width,
+  maxWidth,
+  color,
+  icon,
+  children,
+  title,
+  pl,
+  height,
+}: SectionTitleWrapperProp) => {
+  return (
+    <Box
+      component="section"
+      sx={{
+        maxWidth,
+        width,
+        pl,
+        height: "100%",
+      }}
+    >
+      {/* title */}
+      <Box
+        component="h3"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          mb: "16px",
+          color,
+          fontSize: "20px",
+          fontWeight: "800",
+          height,
+        }}
+      >
+        <Box
+          component="span"
+          sx={{
+            display: "inline-flex",
+            width: "35px",
+            height: "35px",
+            borderRadius: "50%",
+            backgroundColor: color,
+            alignItems: "center",
+            justifyContent: "center",
+            mr: "4px",
+          }}
+        >
+          <FontAwesomeIcon icon={icon} color="#ffffff" />
+        </Box>
+        {title}
+      </Box>
+      {children}
+    </Box>
+  )
+}
+
+export default SectionTitleWrapper
