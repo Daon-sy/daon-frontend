29d62a3e52410ceb9655a9157e6eb6506c5ecd96
sungeunnn <fronteun@naver.com>
2023-12-07 11:13:12 +0900

    🔨[refactor] useFetchWorkspaceList에 myWorkspaceId 추가 #89

--- src/hooks/workspace/useFetchWorkspaceList.ts
+++ src/hooks/workspace/useFetchWorkspaceList.ts
@@ -3,17 +3,27 @@ import axios from "axios"
 import { ErrorResponse } from "api"
 import { workspaceListApi } from "api/workspace"
 import { Workspace } from "_types/workspace"
+import { getMyWorkspaceIdStore } from "store/userStore"
 
 const useFetchWorkspaceList = (skip = false) => {
   const [workspaces, setWorkspaces] = React.useState<Workspace[]>([])
   const [isFetching, setIsFetching] = React.useState(false)
+  const { setMyWorkspaceId } = getMyWorkspaceIdStore()
   const [error, setError] = React.useState<ErrorResponse>()
 
   const fetchWorkspaceList = async () => {
     try {
       setIsFetching(true)
       const { data } = await workspaceListApi()
+      const fetchedWorkspaces = data.workspaces
       setWorkspaces(data.workspaces)
+
+      const personalWorkspace = fetchedWorkspaces.find(
+        workspace => workspace.division === "PERSONAL",
+      )
+      if (personalWorkspace) {
+        setMyWorkspaceId(personalWorkspace?.workspaceId)
+      }
     } catch (e) {
       if (axios.isAxiosError(e)) {
         const { response } = e
