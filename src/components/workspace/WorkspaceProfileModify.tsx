8f5e5542228d9fe6119bed85665869ecef1412bb
sungeunnn <fronteun@naver.com>
2023-12-07 11:16:17 +0900

    ❇️[feat] MenuModal 삭제버튼 추가 및 설정모달 스타일 #89 #105

--- src/components/workspace/WorkspaceProfileModify.tsx
+++ src/components/workspace/WorkspaceProfileModify.tsx
@@ -4,12 +4,13 @@ import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
 import { useAlert } from "hooks/useAlert"
 import ImageInput from "components/image/ImageInput"
-import { getWorkspaceStore } from "store/userStore"
+import { getMyWorkspaceIdStore, getWorkspaceStore } from "store/userStore"
 import { imageUploadApi } from "api/image"
 import {
   modifyMyWorkspaceParticipantInfoApi,
   ModifyMyWorkspaceParticipantInfoRequestBody,
   myWorkspaceParticipantDetailApi,
+  resetPersonalWorkspaceApi,
   withdrawWorkspaceApi,
 } from "api/workspace"
 import useImageUrlInputRef from "hooks/useImageUrlInputRef"
@@ -21,12 +22,15 @@ import { useNavigate } from "react-router-dom"
 
 const WorkspaceProfileModify = () => {
   const { workspace, myProfile, setMyProfile } = getWorkspaceStore()
+  const { myWorkspaceId } = getMyWorkspaceIdStore()
   const workspaceId = workspace?.workspaceId
   const { addSuccess } = useAlert()
   const navigate = useNavigate()
   const [ref, changeRef] = useImageUrlInputRef()
   const [memberEmails, setMemberEmails] = React.useState<Array<MemberEmail>>([])
   const [workspaceWithdrawModalOpen, setWorkspaceWithdrawModalOpen] =
+    React.useState(false)
+  const [resetPersonalWorkspaceModalOpen, setResetPersonalWorkspaceModalOpen] =
     React.useState(false)
   const fetchMemberEmails = async () => {
     const { data } = await myEmailsApi()
@@ -68,10 +72,17 @@ const WorkspaceProfileModify = () => {
 
   const withdrawWorkspace = async () => {
     if (workspaceId) {
-      await withdrawWorkspaceApi(workspaceId)
+      withdrawWorkspaceApi(workspaceId)
       addSuccess("워크스페이스를 탈퇴하였습니다")
       // 개인워크스페이스로 이동
-      navigate("workspace/1")
+      navigate(`/workspace/${myWorkspaceId}`)
+    }
+  }
+
+  const resetPersonalWorkspace = async () => {
+    if (workspaceId) {
+      await resetPersonalWorkspaceApi(workspaceId)
+      addSuccess("초기화 완료")
     }
   }
 
@@ -163,27 +174,54 @@ const WorkspaceProfileModify = () => {
           </Box>
         </Box>
       </Stack>
-
-      <Box mt={1} sx={{ position: "absolute", bottom: 10, right: 0 }}>
-        <Button
-          sx={{ color: "#c9c9c9" }}
-          onClick={() => setWorkspaceWithdrawModalOpen(true)}
-        >
-          워크스페이스 탈퇴
-        </Button>
-      </Box>
-      {workspaceWithdrawModalOpen ? (
-        <ConfirmDialog
-          open={workspaceWithdrawModalOpen}
-          maxWidth="xs"
-          handleConfirm={withdrawWorkspace}
-          handleClose={() => {
-            setWorkspaceWithdrawModalOpen(false)
-          }}
-        >
-          정말로 이 워크스페이스를 탈퇴하시겠습니까?
-        </ConfirmDialog>
-      ) : null}
+      {workspace.division !== "GROUP" ? (
+        <Box>
+          <Box mt={1} sx={{ position: "absolute", bottom: 10, right: 10 }}>
+            <Button
+              sx={{ color: "#c9c9c9" }}
+              color="error"
+              onClick={() => setResetPersonalWorkspaceModalOpen(true)}
+            >
+              초기화하기
+            </Button>
+          </Box>
+          {resetPersonalWorkspaceModalOpen ? (
+            <ConfirmDialog
+              open={resetPersonalWorkspaceModalOpen}
+              maxWidth="xs"
+              handleConfirm={resetPersonalWorkspace}
+              handleClose={() => {
+                setResetPersonalWorkspaceModalOpen(false)
+              }}
+            >
+              정말로 이 워크스페이스를 초기화 하시겠습니까?
+            </ConfirmDialog>
+          ) : null}
+        </Box>
+      ) : (
+        <Box>
+          <Box mt={1} sx={{ position: "absolute", bottom: 10, right: 10 }}>
+            <Button
+              sx={{ color: "#c9c9c9" }}
+              onClick={() => setWorkspaceWithdrawModalOpen(true)}
+            >
+              워크스페이스 탈퇴
+            </Button>
+          </Box>
+          {workspaceWithdrawModalOpen ? (
+            <ConfirmDialog
+              open={workspaceWithdrawModalOpen}
+              maxWidth="xs"
+              handleConfirm={withdrawWorkspace}
+              handleClose={() => {
+                setWorkspaceWithdrawModalOpen(false)
+              }}
+            >
+              정말로 이 워크스페이스를 탈퇴하시겠습니까?
+            </ConfirmDialog>
+          ) : null}
+        </Box>
+      )}
     </Box>
   )
 }
