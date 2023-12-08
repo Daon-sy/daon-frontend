610fbeb7d84196230a56bab139bcd7bf7c3668e0
hi0yoo <yoo971202@naver.com>
2023-12-08 11:45:22 +0900

    🐞[fix] 새로고침시 토큰 재발급 오류 해결

--- src/auth/RefreshComponent.tsx
+++ src/auth/RefreshComponent.tsx
@@ -7,22 +7,19 @@ const RefreshComponent = ({ children }: React.PropsWithChildren) => {
   const BEARER_TYPE = "Bearer "
   const tokenStore = getTokenStore()
 
-  React.useEffect(
-    () => () => {
-      basicAxios
-        .post("/api/auth/reissue")
-        .then((res: AxiosResponse) => {
-          const { headers } = res.headers
-          const authHeader: string = headers.authorization
-          if (!!authHeader && authHeader.startsWith(BEARER_TYPE)) {
-            const accessToken = authHeader.substring(BEARER_TYPE.length)
-            tokenStore.setToken(accessToken)
-          }
-        })
-        .catch(error => error)
-    },
-    [],
-  )
+  React.useEffect(() => {
+    basicAxios
+      .post("/api/auth/reissue")
+      .then((res: AxiosResponse) => {
+        const { headers } = res.headers
+        const authHeader: string = headers.authorization
+        if (!!authHeader && authHeader.startsWith(BEARER_TYPE)) {
+          const accessToken = authHeader.substring(BEARER_TYPE.length)
+          tokenStore.setToken(accessToken)
+        }
+      })
+      .catch(error => error)
+  }, [])
 
   return <div>{children}</div>
 }
