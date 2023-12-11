9a469f0744fa7bfffe61d84a60d6949a9c4715b5
sungeunnn <fronteun@naver.com>
2023-12-11 20:24:53 +0900

    🔨[refactor] 워크스페이스 공지사항 삭제  #168
    - 삭제 후 목록조회 -> 단건조회 변경

--- src/hooks/workspace/useRemoveWorkspaceNotice.ts
+++ src/hooks/workspace/useRemoveWorkspaceNotice.ts
@@ -3,18 +3,21 @@ import axios from "axios"
 import { ErrorResponse } from "api"
 import { removeWorkspaceNoticeApi } from "api/workspaceNotice"
 import { useAlert } from "hooks/useAlert"
-import useFetchWorkspaceNoticeList from "./useFetchWorkspaceNoticeList"
+import useFetchWorkspaceNoticeDetail from "./useFetchWorkspaceNoticeDetail"
 
 const useRemoveWorkspaceNotice = (workspaceId: number, noticeId: number) => {
   const [error, setError] = React.useState<ErrorResponse>()
   const { addSuccess } = useAlert()
-  const { fetchWorkspaceNoticeList } = useFetchWorkspaceNoticeList(workspaceId)
+  const { fetchWorkspaceNoticeDetail } = useFetchWorkspaceNoticeDetail(
+    workspaceId,
+    noticeId,
+  )
 
   const removeNotice = async () => {
     try {
       await removeWorkspaceNoticeApi(workspaceId, noticeId)
       addSuccess("공지사항이 삭제되었습니다.")
-      fetchWorkspaceNoticeList()
+      fetchWorkspaceNoticeDetail()
     } catch (e) {
       if (axios.isAxiosError(e)) {
         const { response } = e
