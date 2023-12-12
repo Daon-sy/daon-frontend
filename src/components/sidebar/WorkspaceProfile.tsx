e2bd0ef2a6fea1fb9a6672f4efd947a50702b10c
sungeunnn <fronteun@naver.com>
2023-12-12 16:14:00 +0900

    💄[style] 사이드바(LNB) 프로필 디자인 수정 #202
    - role 아이콘 변경
    - 전반적인 위치 조정

--- src/components/sidebar/WorkspaceProfile.tsx
+++ src/components/sidebar/WorkspaceProfile.tsx
@@ -3,14 +3,13 @@ import { Box } from "@mui/material"
 import { getWorkspaceStore } from "store/userStore"
 import WorkspaceProfileModifyModal from "components/workspace/modal/WorkspaceProfileModifyModal"
 import EditIcon from "@mui/icons-material/Edit"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCrown, faLeaf } from "@fortawesome/free-solid-svg-icons"
 import ColorAvatar from "components/common/ColorAvatar"
+import wsIcon from "assets/img/ws_icon.png"
+import pjIcon from "assets/img/pj_icon.png"
 import SubIconBtn from "./SubIconBtn"
 
 const WorkSpaceProfile: React.FC = () => {
   let icon = null
-  let color = null
   const { myProfile } = getWorkspaceStore()
   const [profileModfiyModalOpen, setProfileModfiyModalOpen] =
     React.useState(false)
@@ -21,17 +20,16 @@ const WorkSpaceProfile: React.FC = () => {
   }
 
   if (myProfile?.role === "WORKSPACE_ADMIN") {
-    icon = faCrown
-    color = "#fdd835"
+    icon = wsIcon
   } else if (myProfile?.role === "PROJECT_ADMIN") {
-    icon = faLeaf
-    color = "#009959"
+    icon = pjIcon
   }
 
   return (
     <Box
       sx={{
         boxSizing: "border-box",
+        paddingX: 1,
         width: "100%",
         height: "20%",
         minHeight: "110px",
@@ -46,57 +44,60 @@ const WorkSpaceProfile: React.FC = () => {
         pl: "12px",
       }}
     >
-      {icon ? (
-        <Box
-          sx={{
-            position: "absolute",
-            top: 5,
-            right: 10,
-            color,
-          }}
-        >
-          <FontAwesomeIcon icon={icon} />
-        </Box>
-      ) : null}
-      <ColorAvatar
-        id={myProfile?.workspaceParticipantId}
-        src={myProfile?.imageUrl}
-        sx={{ width: 65, height: 65 }}
+      <SubIconBtn
+        color="#ffbe00"
+        onClick={openProfileModifyModal}
+        icon={<EditIcon sx={{ width: 20, height: 20, p: 0.5 }} />}
+        position="absolute"
+        top="4px"
+        right="4px"
       />
+      <Box sx={{ position: "relative", mr: 1 }}>
+        {icon ? (
+          <Box
+            component="img"
+            sx={{
+              position: "absolute",
+              width: "24px",
+              height: "24px",
+              zIndex: 10,
+              bottom: 2,
+              right: 0,
+            }}
+            src={icon}
+          />
+        ) : null}
+        <ColorAvatar
+          id={myProfile?.workspaceParticipantId}
+          src={myProfile?.imageUrl}
+          sx={{ width: 65, height: 65 }}
+        />
+      </Box>
       <Box
         sx={{
-          width: "55%",
+          width: "60%",
           mt: "2px",
           textAlign: "left",
           color: "white",
           position: "relative",
         }}
       >
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Box
-            sx={{
-              maxWidth: "100px",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              whiteSpace: "nowrap",
-              wordBreak: "break-all",
-              borderRadius: 1,
-              fontSize: 20,
-              fontWeight: "bold",
-              display: "block",
-              mt: 2,
-            }}
-          >
-            {myProfile?.name}
-          </Box>
-          <SubIconBtn
-            color="#808080"
-            onClick={openProfileModifyModal}
-            icon={<EditIcon />}
-            position="relative"
-            top="8px"
-          />
+        <Box
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            wordBreak: "break-all",
+            borderRadius: 1,
+            fontSize: 14,
+            fontWeight: "bold",
+            display: "block",
+            mt: 2,
+          }}
+        >
+          {myProfile?.name}
         </Box>
+
         <Box
           sx={{
             borderRadius: 1,
@@ -105,7 +106,7 @@ const WorkSpaceProfile: React.FC = () => {
             textOverflow: "ellipsis",
             whiteSpace: "nowrap",
             color: "#b6d1c2",
-            marginY: 1,
+            marginY: 0.5,
             fontSize: 12,
             lineHeight: "20px",
             fontWeight: "bold",
